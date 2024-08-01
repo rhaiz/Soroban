0435351c5a33f773d5a106c4516712789cf88e04
RhaissaZeferino <rhaissazeferino@gmail.com>
2024-07-31 22:35:57 -0300

    Add select to define soroban length

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
 import Keta from './components/Keta/Keta';
 import DecimalTable from './components/DecimalTable/DecimalTable'
 
 function App() {
-  const elements = Array.from({ length: 11 });
-  const numberTds = 11
+  const [elementsLength, setElementsLength] = useState(9);
+  const [numberTds, setNumberTds] = useState(9);
+
+  const handleDropdownChange = (event) => {
+    const newValue = parseInt(event.target.value);
+    if (newValue >= 4 && newValue <= 17) {
+      setElementsLength(newValue);
+      setNumberTds(newValue);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -13,11 +22,18 @@ function App() {
         </p>
       </header>
       <body>
+      <select value={elementsLength} onChange={handleDropdownChange}>
+          {Array.from({ length: 14 }, (_, index) => (
+            <option key={index + 4} value={index + 4}>
+              {index + 4}
+            </option>
+          ))}
+        </select>
         <DecimalTable numberTds={numberTds} />
         <div className='waku'>
-          {elements.map((_, index) => (
+          {Array.from({ length: elementsLength }, (_, index) => (
             <Keta key={index} />
-            ))}
+          ))}
         </div>
       </body>
     </div>
